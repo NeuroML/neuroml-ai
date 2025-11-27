80dad73e5385a8154d8979a84d2d59b7a7cc6a00
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2025-11-27 11:09:16 +0000

    chore: add script to start mpc server

---
+++ scripts/devstack.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Copyright 2025 Ankur Sinha
+# Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com> 
+# File : 
+#
+
+
+uv pip install -e .[dev]
+
+echo "Re-starting MCP server"
+pgrep -fa nml-mcp && pkill -f --signal SIGINT nml-mcp || echo "No running NeuroML MCP instance found"
+nml-mcp &
+
+
